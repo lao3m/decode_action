54d177e1db4b4276848840ba1b3b351fabf0bf65
action <action@github.com>
2026-06-05 12:39:47 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,6 +1,6 @@
-//Fri Jun 05 2026 07:25:35 GMT+0000 (Coordinated Universal Time)
+//Fri Jun 05 2026 12:39:47 GMT+0000 (Coordinated Universal Time)
 //Base:<url id="cv1cref6o68qmpt26ol0" type="url" status="parsed" title="GitHub - echo094/decode-js: JS混淆代码的AST分析工具 AST analysis tool for obfuscated JS code" wc="2165">https://github.com/echo094/decode-js</url>
 //Modify:<url id="cv1cref6o68qmpt26olg" type="url" status="parsed" title="GitHub - smallfawn/decode_action: 世界上本来不存在加密，加密的人多了，也便成就了解密" wc="741">https://github.com/smallfawn/decode_action</url>
-(function (_0x4dd702) {
-  _0x4dd702.serverParams.u = md5(_0x4dd702.scene.code + _0x4dd702.scene.createTime + _0x4dd702.scene.code + _0x4dd702.scene.code + _0x4dd702.scene.userId + _0x4dd702.serverParams.u + _0x4dd702.scene.id + "6sglk023z");
+(function (_0x124c37) {
+  _0x124c37.serverParams.u = md5(_0x124c37.scene.userId + "xoy016mc" + _0x124c37.scene.id + _0x124c37.scene.userId + _0x124c37.scene.code + _0x124c37.scene.createTime + _0x124c37.scene.userId + _0x124c37.serverParams.u + "9xbqp7cv");
 })(window);
